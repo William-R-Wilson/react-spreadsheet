ada9f2cfbb37b02e768ed0e0f7e469f1b35fa7f6
Will <williamrwilson@gmail.com>
2019-08-11 14:50:22 -0400

    initial layout working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const headers = ["A", "B", "C", "D", "E"];
+    return (
+      <Spreadsheet headers={headers} />
+      );
+  }
+}
+
+class Spreadsheet extends React.Component {
+   render() {
+      const headerData = this.props.headers
+      console.log("headerData: " + headerData);
+      console.log("this.props.headers: " + this.props.headers);
+      return(
+        <table className="table">
+          <thead>
+            <TableHeader headers={headerData} />
+          </thead>
+          <tbody>
+            <Line headers={headerData} />
+          </tbody>
+        </table>
+      )
+    }
+  }
+
+const TableHeader = props => {
+    console.log("props inside TableHeader: " + props.headers);
+    const header = props.headers.map((name, index) => {
+    return(
+          <th key={index} >{name}</th>
+        )
+
+  })
+  return(
+    <tr>
+      {header}
+    </tr>
+  )
+}
+
+
+const Line = props => {
+  const row = props.headers.map((name, index) => {
+    return (
+        <TableInput key={index} />
+    )
+  })
+  return (<tr>{row}</tr>)
+}
+
+class TableInput extends React.Component {
+  render() {
+    return (
+      <td className="border border-dark">
+        <input type="text" className="form-control spreadsheet-input" />
+      </td>
+    )
+  }
 }
 
 export default App;
